90770ff87e65f6410661cae5c9823680dc3fc0cb
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-04-29 21:36:45 -0400

    imported inquirer and generateMarkDown.js into index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkDown');
+const fs = require('fs')
 
+const data = {
+    title: '',
+    description: '',
+    installation: '',
+    usage: '',
+    license: '',
+    contributions: '',
+    test: '',
+    github: '',
+    email: ''
+}
 // TODO: Create an array of questions for user input
 const questions = [];
 
